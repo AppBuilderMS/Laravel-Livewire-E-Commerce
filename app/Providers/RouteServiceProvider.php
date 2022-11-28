6f57dee504e34b58f6cb1606eab56dac19fbd751
Your Name <you@example.com>
2022-11-28 19:23:19 +0200

    add logo.svg

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,7 +18,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/';
-    public const DASHBOARD = '/admin/dashboard'; 
+    public const DASHBOARD = '/admin/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
